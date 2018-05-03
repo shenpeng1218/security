6a5b84cd9ef8f22628d83a58079b0090b614a7c9
shenpeng <shenpeng@lvmama.com>
2018-05-03 19:35:01 +0800

    first commit ! ! !

---
+++ security-controller/src/main/java/com/moss/Application.java
@@ -0,0 +1,14 @@
+package com.moss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shenpeng on 2018/5/3.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
